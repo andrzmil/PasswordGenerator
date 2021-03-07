2e4d5b9b6e7058611d11106a3d49f4c16585e728
andrzmil <andrz.milewski@gmail.com>
2021-03-07 18:31:57 +0100

    added ucase, lcase, digits, punct, sum validation

--- PG_GUI.py
+++ PG_GUI.py
@@ -4,17 +4,22 @@ from tkinter.ttk import *
 from PG_MAIN import *
 
 root = Tk()
-root.geometry("400x100")
+root.geometry("400x150")
 root.title("Password Generator")
 
-
+def cur_value():
+    pwd_len.config(from_=int(upcase_len.get())+int(lowcase_len.get())+int(dgt_len.get())+int(punct_len.get()))
+    upcase_len.config(to=int(pwd_len.get())-int(lowcase_len.get())-int(dgt_len.get())-int(punct_len.get()))
+    lowcase_len.config(to=int(pwd_len.get())-int(upcase_len.get())-int(dgt_len.get())-int(punct_len.get()))
+    dgt_len.config(to=int(pwd_len.get())-int(upcase_len.get())-int(lowcase_len.get())-int(punct_len.get()))
+    punct_len.config(to=int(pwd_len.get())-int(upcase_len.get())-int(lowcase_len.get())-int(dgt_len.get()))
 
 #password length
 cur_pwd_length = Variable(value=16)
 pwd_len_lbl = Label(root, text="Enter password length: ")
 pwd_len_lbl.grid(column = 0, row = 0)
 
-pwd_len = Spinbox(root, from_=8, to=40, textvariable= cur_pwd_length)
+pwd_len = Spinbox(root, from_=8, to=70, textvariable= cur_pwd_length, state = "readonly")
 pwd_len.grid(column = 1, row = 0)
 
 #uppercase
@@ -22,7 +27,7 @@ cur_upcase_len = Variable(value=1)
 upcase_lbl = Label(root, text="Enter number of uppercase letters: ")
 upcase_lbl.grid(column = 0, row = 1)
 
-upcase_len = Spinbox(root, from_=0, to=40,textvariable= cur_upcase_len)
+upcase_len = Spinbox(root, from_=0, to=40,textvariable= cur_upcase_len, command = cur_value, state = "readonly")
 upcase_len.grid(column = 1, row = 1)
 
 #lowercase
@@ -30,35 +35,45 @@ cur_lowcase_len = Variable(value=1)
 lowcase_lbl = Label(root, text="Enter number of lowercase letters: ")
 lowcase_lbl.grid(column = 0, row = 2)
 
-lowcase_len = Spinbox(root, from_=0, to=40, textvariable= cur_lowcase_len)
+lowcase_len = Spinbox(root, from_=0, to=40, textvariable= cur_lowcase_len, command = cur_value, state = "readonly")
 lowcase_len.grid(column = 1, row = 2)
 
-# #digits
-# dgt_lbl = Label(root, text="Enter number of digits: ")
-# dgt_lbl.grid(column = 0, row = 3)
+#digits
+cur_dgt_len = Variable(value=1)
+dgt_lbl = Label(root, text="Enter number of digits: ")
+dgt_lbl.grid(column = 0, row = 3)
 
-# dgt_len = Spinbox(root, from_=0, to = max_number(), textvariable=  Variable(value = 0))
-# dgt_len.grid(column = 1, row = 3)
+dgt_len = Spinbox(root, from_=0, to = 40, textvariable = cur_dgt_len, command = cur_value, state = "readonly")
+dgt_len.grid(column = 1, row = 3)
 
+#punct
+cur_punct_len = Variable(value=1)
+punct_lbl = Label(root, text="Enter number of punct: ")
+punct_lbl.grid(column = 0, row = 4)
 
-
-
-
-
-# def generate_button():
-#     pwd_lbl.config(text=generate_password(int(pwd_len.get())))
-#     print(pwd_len.get())
-
-# #password label
-# pwd_lbl = Label(root)
-# pwd_lbl.pack(side = "top")
+punct_len = Spinbox(root, from_=0, to = 40, textvariable = cur_punct_len, command = cur_value, state = "readonly")
+punct_len.grid(column = 1, row = 4)
 
 
 
 
-# #add button
-# generate = Button(root, text = "Generate", command=generate_button)
-# generate.pack(side = "bottom")
+
+def generate_button():
+     pwd_lbl.delete(0,END)
+     pwd_lbl.insert(0,generate_password(int(pwd_len.get()), int(upcase_len.get()), 
+     int(lowcase_len.get()), int(dgt_len.get()), int(punct_len.get())))
+     print(pwd_len.get())
+
+#password label
+pwd_lbl = Entry(root)
+pwd_lbl.grid(row = 5, column=0, columnspan = 2, ipadx=130)
+
+
+
+
+#add button
+generate = Button(root, text = "Generate", command=generate_button)
+generate.grid(column = 0, row = 6)
 
 
 root.mainloop()
--- PG_MAIN.py
+++ PG_MAIN.py
@@ -1,20 +1,35 @@
 import string
 import numpy as np
 
-def generate_password(length):
-    lcase = list(string.ascii_lowercase)
-    ucase = list(string.ascii_uppercase)
+def generate_password(length, lc, uc, nb, pct):
+    lcase = list(string.ascii_uppercase)
+    ucase = list(string.ascii_lowercase)
     numbers = list(string.digits)
     punct = list(string.punctuation)
 
     all_chars = lcase + ucase + numbers + punct
     generated_pwd = list()
 
-    for i in range(length):
+    for i in range(length-lc-uc-nb-pct):
         generated_pwd.append(np.random.choice(all_chars))   
 
-    print(generated_pwd)
+    for i in range(lc):
+        generated_pwd.append(np.random.choice(lcase)) 
+
+    for i in range(uc):
+        generated_pwd.append(np.random.choice(ucase))
+
+    for i in range(nb):
+        generated_pwd.append(np.random.choice(numbers)) 
+
+    for i in range(pct):
+        generated_pwd.append(np.random.choice(punct))
+
+    np.random.shuffle(generated_pwd)
+    
 
     return("".join(generated_pwd))
+
+
 
 print("asas")
